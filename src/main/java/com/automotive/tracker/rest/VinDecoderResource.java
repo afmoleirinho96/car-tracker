8ebc310c0eccfeb73db9877f053aa6f8b54bb7cc
afmoleirinh96 <andrewacm25@gmail.com>
2020-09-20 18:59:12 +0100

    Add VehicleDecoderResource Implement Service logic to fetch VehicleInfo and VehicleDecode.

---
+++ src/main/java/com/automotive/tracker/rest/VinDecoderResource.java
@@ -0,0 +1,32 @@
+package com.automotive.tracker.rest;
+
+import com.automotive.tracker.services.VehicleService;
+import com.automotive.tracker.services.VinDecoderService;
+import com.automotive.tracker.to.rest.vindecode.VinDecodeDto;
+import com.automotive.tracker.to.rest.vindecode.VinInfoDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("vehicles/{vehicleId}")
+public class VinDecoderResource {
+
+    private final VinDecoderService vinDecoderService;
+
+    public VinDecoderResource(VehicleService vehicleService, VinDecoderService vinDecoderService) {
+        this.vinDecoderService = vinDecoderService;
+    }
+
+    @GetMapping(path = "vin-info")
+    public VinInfoDto getVinInfo(@PathVariable String vehicleId) {
+        return vinDecoderService.getVinInfo(vehicleId);
+    }
+
+    @GetMapping(path = "vin-info-decoded", produces = "application/json")
+    public VinDecodeDto getVinInfoDecoded(@PathVariable String vehicleId) {
+        return vinDecoderService.decodeVinInfo(vehicleId);
+    }
+
+}
---
+++ src/main/java/com/automotive/tracker/services/VinDecoderService.java
@@ -0,0 +1,62 @@
+package com.automotive.tracker.services;
+
+import com.automotive.tracker.exceptions.VehicleNotFoundException;
+import com.automotive.tracker.mapper.VinDecodeInfoMapper;
+import com.automotive.tracker.mapper.VinDecodeMapper;
+import com.automotive.tracker.model.Vehicle;
+import com.automotive.tracker.repository.VehicleRepository;
+import com.automotive.tracker.services.providers.VinDecoderProvider;
+import com.automotive.tracker.to.rest.vindecode.VinDecodeDto;
+import com.automotive.tracker.to.rest.vindecode.VinInfoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class VinDecoderService {
+
+    private static final String decodeInfoUrl = "/decode/info/";
+    private static final String decodeUrl = "/decode/";
+
+    private final VinDecoderProvider vinDecoderProvider;
+    private final VehicleRepository vehicleRepository;
+    private final VinDecodeInfoMapper vinDecodeInfoMapper;
+    private final VinDecodeMapper vinDecodeMapper;
+
+    public VinDecoderService(VinDecoderProvider vinDecoderProvider, VehicleRepository vehicleRepository,
+                             VinDecodeInfoMapper vinDecodeInfoMapper, VinDecodeMapper vinDecodeMapper) {
+        this.vinDecoderProvider = vinDecoderProvider;
+        this.vehicleRepository = vehicleRepository;
+        this.vinDecodeInfoMapper = vinDecodeInfoMapper;
+        this.vinDecodeMapper = vinDecodeMapper;
+    }
+
+    public VinInfoDto getVinInfo(String vehicleId) {
+        String vehicleVin = fetchVehicleVin(vehicleId);
+
+        return Optional.ofNullable(vinDecoderProvider.vinDecodeInfo(decodeInfoUrl, vehicleVin))
+                       .map(vinDecodeInfoMapper::map)
+                       .orElseThrow(VehicleNotFoundException.supplyVinDecodeNotFound(vehicleVin));
+    }
+
+    public VinDecodeDto decodeVinInfo(String vehicleId) {
+        String vehicleVin = fetchVehicleVin(vehicleId);
+
+        return Optional.ofNullable(vinDecoderProvider.vinDecode(decodeUrl, vehicleVin))
+                       .map(vinDecode -> vinDecodeMapper.map(vinDecode, vinDecode.getBalance()))
+                       .orElseThrow(VehicleNotFoundException.supplyVinDecodeNotFound(vehicleVin));
+    }
+
+    private String fetchVehicleVin(String vehicleId) {
+        Vehicle vehicle = vehicleRepository.findById(vehicleId)
+                                           .orElseThrow(VehicleNotFoundException.supplyVehicleNotFound(vehicleId));
+
+        String vehicleVin = vehicle.getVin();
+
+        if (vehicleVin == null) {
+            throw VehicleNotFoundException.vehicleWithVinNotFound(vehicleVin);
+        }
+
+        return vehicleVin;
+    }
+}
