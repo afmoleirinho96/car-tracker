7f1f4dddd8b6c68e3d167ff72a52e0a8280db1c2
afmoleirinh96 <andrewacm25@gmail.com>
2020-09-24 02:55:28 +0100

    Rename class.

--- src/main/java/com/automotive/tracker/configuration/RestTemplateFactory.java
+++ src/main/java/com/automotive/tracker/configuration/RestTemplateFactory.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestTemplateFactory {
 
     @Bean
     @VinDecoderApi
