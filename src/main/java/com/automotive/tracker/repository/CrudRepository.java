e462bd97c22cd9f056aed5a3a0aecf6fccc66ee5
afmoleirinh96 <andrewacm25@gmail.com>
2020-09-24 02:50:26 +0100

    Add Swagger gradle properties. Create SwaggerConfig. Remove unused method.

--- src/main/java/com/automotive/tracker/repository/CrudRepository.java
+++ src/main/java/com/automotive/tracker/repository/CrudRepository.java
@@ -8,13 +8,10 @@ public interface CrudRepository<T, ID> {
 
     <S extends T> S save(S entity);
 
-    // iterable in CrudRepository
     Stream<T> findAll();
 
-    // avoid the use of null checks
     Optional<T> findById(ID id);
 
     void deleteById(ID id);
 
-    void deleteAll();
 }
